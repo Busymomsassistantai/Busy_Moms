5a2f9d2b4dbae25c4b0402c8b8fb6b3ae08dfada
daniel5088 <146386113+daniel5088@users.noreply.github.com>
2025-08-16 18:36:29 -0400

    Add solid white background to navigation bar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -17,7 +17,7 @@ export function Navigation({ currentScreen, onScreenChange }: NavigationProps) {
   ];
 
   return (
-    <div className="absolute bottom-0 left-0 right-0 bg-white border-t border-gray-200">
+    <div className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 z-50">
       <div className="flex items-center justify-around py-2">
         {navItems.map((item) => (
           <button
